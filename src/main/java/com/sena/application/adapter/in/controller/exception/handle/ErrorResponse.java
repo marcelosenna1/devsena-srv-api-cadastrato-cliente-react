c271c9044f5cf98b884c15293bfe143be9fa1f26
Marcelo Sena <marcelomarcos2@gmail.com>
2025-04-08 18:41:39 -0300

    [feature]: criação de captura global de exceções

---
+++ src/main/java/com/sena/application/adapter/in/controller/exception/handle/ErrorResponse.java
@@ -0,0 +1,6 @@
+package com.sena.application.adapter.in.controller.exception.handle;
+
+
+public record ErrorResponse(String message, int status, long timestamp) {
+}
+
--- src/main/java/com/sena/application/adapter/out/repository/entity/ClienteEntity.java
+++ src/main/java/com/sena/application/adapter/out/repository/entity/ClienteEntity.java
@@ -1,6 +1,9 @@
 package com.sena.application.adapter.out.repository.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "cliente")
@@ -8,8 +11,15 @@ public record ClienteEntity (
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         Long id,
+
+        @NotNull(message = "Nome não deve ser preenchido")
         String nome,
+
+        @NotNull
+        @Min(value = 18, message = "Idade deve ser maior ou igual a 18")
         Integer idade,
+
+        @Email(message = "Email inválido")
         String email
 ) {
 }
