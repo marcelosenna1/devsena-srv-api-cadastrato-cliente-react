9d4561b35db72e73764812564dbda722c2830a00
Marcelo Sena <marcelomarcos2@gmail.com>
2025-04-08 18:11:29 -0300

    [fix]: ajustes no controller, renomeio e adição de anotations

--- src/main/java/com/sena/application/adapter/in/controller/ClienteController.java
+++ src/main/java/com/sena/application/adapter/in/controller/ClienteController.java
@@ -2,29 +2,30 @@ package com.sena.application.adapter.in.controller;
 
 import com.sena.application.core.domain.Cliente;
 import com.sena.application.core.port.in.ClienteInputPort;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/cadastro")
-public class CadastroController {
+@RequestMapping("/api/v1/cliente")
+public class ClienteController {
 
     private final ClienteInputPort service;
 
-    public CadastroController(ClienteInputPort service) {
+    public ClienteController(ClienteInputPort service) {
         this.service = service;
     }
 
     @PostMapping
-    public ResponseEntity<Cliente> cadastrarCliente(Cliente cliente) {
+    public ResponseEntity<Cliente> cadastrarCliente(@Valid @RequestBody Cliente cliente) {
         var clienteCadastrado = service.cadastrarCliente(cliente);
         return ResponseEntity.ok(clienteCadastrado);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletarCliente(Long id) {
+    public ResponseEntity<Void> deletarCliente(@PathVariable Long id) {
         service.deletarCliente(id);
         return ResponseEntity.noContent().build();
     }
